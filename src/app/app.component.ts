a41bc8275bf206f02a6637615ca6d9ecc0492398
= <=>
2023-05-02 19:36:45 -0400

    traduccion de la pagina principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Momotaro Food';
+
+  constructor(private title: Title){
+    title.setTitle("Momotaro Foods")
+  }
 }
