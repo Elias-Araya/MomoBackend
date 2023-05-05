a47d836260d2a4e94a9f4f5db13d033d44f8cdb2
= <=>
2023-05-04 22:31:22 -0400

    componente home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IntranetComponent } from './intranet/intranet.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+const routes : Routes = [
+  {
+    path:'', component: AppComponent
+  },
+  {
+    path:'login', component: LoginComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     IntranetComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
